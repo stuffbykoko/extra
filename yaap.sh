091a71d6887f1ff6a1d00cec564cd98d83e7bf8b
koko-07870 <ko282ko82@gmail.com>
2025-02-06 00:25:18 +0530

    Create yaap.sh

---
+++ yaap.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+
+git clone https://github.com/stuffbykoko/local_manifests --depth 1 -b yaap .repo/local_manifests
+
+/opt/crave/resync.sh
+
+rm -rf vendor/yaap/signing/keys
+git clone https://gitea.com/koko-07870/tmp -b tmp vendor/yaap/signing/keys
+export BUILD_USERNAME=koko-07870
+export BUILD_HOSTNAME=crave
+
+echo "LET'S GO!"
+
+source build/envsetup.sh
+
+lunch yaap_a52q-user && m installclean && m yaap
