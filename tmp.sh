b864c1741f8e50ef8f006edcae540f99d477198e
koko-07870 <ko282ko82@gmail.com>
2025-02-07 02:02:19 +0530

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -6,7 +6,7 @@ repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
 git clone https://github.com/stuffbykoko/local_manifests --depth 1 -b yaap .repo/local_manifests
 
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+repo sync -j$(nproc --all) --force-sync --no-tags --no-clone-bundle --current-branch
 
 rm -rf vendor/yaap/signing/keys
 git clone https://gitea.com/koko-07870/tmp -b tmp vendor/yaap/signing/keys
