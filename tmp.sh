31f4f066b3da40651f0d4a9b08f48a7cf5d590df
koko-07870 <ko282ko82@gmail.com>
2025-02-06 19:32:54 +0530

    Create tmp.sh

---
+++ tmp.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
+
+git clone https://github.com/stuffbykoko/local_manifests --depth 1 -b yaap .repo/local_manifests
+
+repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+
+rm -rf vendor/yaap/signing/keys
+git clone https://gitea.com/koko-07870/tmp -b tmp vendor/yaap/signing/keys
+
+echo "LET'S GO!"
+
+source build/envsetup.sh
+
+lunch yaap_a52q-user && m installclean && m yaap
