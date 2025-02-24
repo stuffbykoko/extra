a6174736421dbe353e005f68cd8a36d0e8d606d3
koko-07870 <ko282ko82@gmail.com>
2025-02-25 00:31:15 +0530

    Update tmp.sh

--- tmp.sh
+++ tmp.sh
@@ -6,7 +6,7 @@ repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
 git clone https://github.com/stuffbykoko/local_manifests -b yaap .repo/local_manifests
 
-repo sync -j$(nproc --all) --no-tags --no-clone-bundle --current-branch
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 echo "LET'S GO!"
 
