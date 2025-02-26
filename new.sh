ece7be61563b68421a1d75eca210d3faf1e7f510
koko-07870 <ko282ko82@gmail.com>
2025-02-26 22:57:18 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/AxionAOSP/android.git -b lineage-22.1 --git-lfs
+
+git clone https://github.com/stuffbykoko/local_manifests -b axion .repo/local_manifests
+
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+
