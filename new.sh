2cf4ca5ddf6dee077099783f69ddac408e441bb1
koko-07870 <ko282ko82@gmail.com>
2025-03-01 18:05:53 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -3,5 +3,3 @@
 repo init -u https://github.com/stuffbykoko/manifest -b vauxite
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
-
-rm -rf vendor/qcom/common/vendor/display/5.15
