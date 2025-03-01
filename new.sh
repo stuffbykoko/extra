79fedbf63d7ced6fbf6a184f19fa2702f562a234
koko-07870 <ko282ko82@gmail.com>
2025-03-01 14:13:57 +0530

    Create new.sh

---
+++ new.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+rm -rf .repo/local_manifests
+
+repo init -u https://github.com/AOSPA/manifest -b vauxite
+
+git clone https://github.com/stuffbykoko/local_manifests -b vauxite .repo/local_manifests
+
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
