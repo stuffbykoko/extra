695dcc5714a87f0eaf5c50c8f369239023d14fc3
koko-07870 <ko282ko82@gmail.com>
2025-03-01 15:29:27 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-rm -rf .repo/local_manifests
-
 repo init -u https://github.com/stuffbykoko/manifest -b vauxite
-
-git clone https://github.com/stuffbykoko/local_manifests -b vauxite .repo/local_manifests
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
