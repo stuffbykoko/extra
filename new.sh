98bafce583bb8cf93c2c22abe78daf30cf384e6a
koko-07870 <ko282ko82@gmail.com>
2025-03-03 15:32:51 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -4,6 +4,8 @@ rm -rf .repo/local_manifests
 
 repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
+git clone https://github.com/stuffbykoko/local_manifests -b yaap .repo/local_manifests
+
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
 echo -e "\e[34m Repo Sync finished \e[0m"
