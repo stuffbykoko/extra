ddb2a2a36c1b814837d600be59be11cd4548a828
koko-07870 <ko282ko82@gmail.com>
2025-03-03 15:26:03 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-repo init -u https://github.com/stuffbykoko/manifest -b vauxite
+repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
