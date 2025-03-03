5651fe01e3e794b9274901f00e22573caeaedd0e
koko-07870 <ko282ko82@gmail.com>
2025-03-03 15:30:53 +0530

    Update new.sh

--- new.sh
+++ new.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+rm -rf .repo/local_manifests
+
 repo init -u https://github.com/yaap/manifest.git -b fifteen --git-lfs
 
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+
+echo -e "\e[34m Repo Sync finished \e[0m"
